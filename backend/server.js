84d0bfb5f2dfc53e21107d592cab8ac36c3f6e15
qfath001 <quhurafathima56@gmail.com>
2024-11-08 09:22:25 -0500

    made changes in server.js and middleware.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ const createAdminUser = require('./createAdmin'); // Import the admin creation s
 const adminPrerequisiteRoutes = require('./adminPrerequisite');
 const adminRoutes = require('./adminRoutes'); // Path to new file
 const { verifyAdmin } = require('./middleware'); // Import your middleware functions
+const { isAuthenticated } = require('./middleware');
 const studentRoutes = require('./studentRoutes'); // To import for student routes
 const advisingRoutes = require('./advisingRoutes');
 const cookieParser = require('cookie-parser');
@@ -94,17 +95,6 @@ const generateOTP = () => {
 
 // Sign-up route with OTP verification
 const otps = {}; // Memory store for OTPs
-
-// Middleware to check if the user is authenticated
-const isAuthenticated = (req, res, next) => {
-  if (req.session && req.session.user) {
-    // Session exists, user is authenticated
-    next();
-  } else {
-    // User is not authenticated
-    res.status(401).json({ message: 'Unauthorized. Please log in.' });
-  }
-};
 
 app.get('/home', (req, res) => {
   if (req.session.user) {
