b89c73a63fe4b4e714ce5e529384b0b98199dbb6
qfath001 <quhurafathima56@gmail.com>
2024-11-11 22:56:23 -0500

    server

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['https://wpproject-frontend.web.app', 'https://wpproject-frontend.firebaseapp.com'], // frontend's URL
+  origin: ['https://wpproject-frontend.web.app'], // frontend's URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only the necessary methods
   credentials: true // Include credentials if needed
 }));
