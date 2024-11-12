63d7ce0ce291ad18a3163914fe5276372895f3a7
qfath001 <quhurafathima56@gmail.com>
2024-11-12 15:14:11 -0500

    comite

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,6 @@ app.use(session({
     secure: false,     // Set to true if using HTTPS in production
     maxAge: 1000 * 60 * 60 * 24,  // Session valid for 1 day
     sameSite: 'none',
-    domain: 'https://wpproject-frontend.web.app'
   }
 }));
 
